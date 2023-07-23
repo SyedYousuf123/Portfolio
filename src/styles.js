a962155260c248f8005d362b31311bccf61c2caa
Syed Yousuf <118276103+SyedYousuf123@users.noreply.github.com>
2023-07-24 02:34:13 +0500

    Delete src directory

--- src/styles.js
+++
@@ -1,9 +0,0 @@
-const styles ={
-    heading1:"text-white my-[20px] sm:text-[75px] font-extrabold",
-    heading2:"text-white leading-tight  font-extrabold ss:text-[75px] text-[60px] capitalize",
-    heading3:"text-white ss:text-[25px] font-extrabold",
-    heading4:"text-DimPink tracking-[2px] ss:text-[20px] font-bold",
-    btn:"bg-secondary text-white rounded-[30px] no-underline border-[2px] border-solid border-transparent hover:bg-transparent hover:border-secondary cursor-pointer transition-all duration-300"
-}
-
-export default styles;
